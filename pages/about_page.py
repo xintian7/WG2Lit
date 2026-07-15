abf1df1f6cd2048e42af0609684f1a1b27eada73
xtian <xin.tian@deltares.nl>
2026-07-15 11:08:39 +0200

    updated the ver number

--- pages/about_page.py
+++ pages/about_page.py
@@ -5,7 +5,7 @@ def render_about_page() -> None:
     st.divider()
     st.markdown("# About")
     st.markdown(
-        "Climate Literature Navigator (ver 0.3a) is a web app developed by the "
+        "Climate Literature Navigator (ver 0.4) is a web app developed by the "
         "[IPCC WGII](https://www.ipcc.ch/working-group/wg2/) TSU to help IPCC authors "
         "find climate-related literature from [OpenAlex](https://openalex.org/)'s database."
     )
